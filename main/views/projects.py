94c03b625e92ed9a73260d0126d2233694c3dd1e
Grzegorz-Bartos <grzes.dzes@gmail.com>
2025-01-25 19:04:02 +0100

    Refractor view and URL names: Changed 'portfolio' to 'projects'

--- main/views/projects.py
+++ main/views/projects.py
@@ -4,14 +4,11 @@ from django.views.generic import ListView
 from django_ratelimit.decorators import ratelimit
 
 from main.models import Project
-from main.utils import get_paginated_queryset
-
-
 
 @method_decorator(ratelimit(key='ip', rate='10/m', block=True), name='dispatch')
-class PortfolioView(ListView):
+class ProjectsView(ListView):
     model = Project
-    template_name = 'portfolio.html'
+    template_name = 'projects.html'
     context_object_name = 'page_obj'
     paginate_by = 10
 
