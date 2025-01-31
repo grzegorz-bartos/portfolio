55547aeb6fc6e1b81bfb46ded6f2f4ed4b5539ef
Grzegorz-Bartos <grzes.dzes@gmail.com>
2025-01-31 14:30:59 +0100

    refactor(cleanup): removed unused import from settings.py

--- core/settings.py
+++ core/settings.py
@@ -1,7 +1,6 @@
 from pathlib import Path
 import os
 import environ
-from django.core.exceptions import ImproperlyConfigured
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
