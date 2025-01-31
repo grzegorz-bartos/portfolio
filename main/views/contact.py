18f692f7483307641dde7dd34c56c9403de7c107
Grzegorz-Bartos <grzes.dzes@gmail.com>
2025-01-31 17:12:51 +0100

    refactor: updated ContactView to properly render contact form

--- main/views/contact.py
+++ main/views/contact.py
@@ -1,8 +1,19 @@
 from django.utils.decorators import method_decorator
-from django.views.generic import TemplateView
+from django.views.generic import View
 from django_ratelimit.decorators import ratelimit
+from django.shortcuts import render, redirect
+from main.models.contact_message import ContactForm
 
 
 @method_decorator(ratelimit(key='ip', rate='10/m', block=True), name='dispatch')
-class ContactView(TemplateView):
-    template_name = 'contact.html'
+class ContactView(View):
+    def get(self, request):
+        form = ContactForm()
+        return render(request, 'contact.html', {'form': form})
+
+    def post(self, request):
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('contact_success')
+        return render(request, 'contact.html', {'form': form})
