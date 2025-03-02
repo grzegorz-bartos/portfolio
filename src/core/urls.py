2934678d09854bb6522d79b40c89d2b392e9b6b1
Grzegorz-Bartos <grzes.dzes@gmail.com>
2025-03-02 22:32:50 +0100

    fix(urls.py): serving media files in development

--- src/core/urls.py
+++ src/core/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import TemplateView
@@ -47,5 +49,5 @@ urlpatterns = [
     path("ckeditor5/", include("django_ckeditor_5.urls")),
 ]
 
-# if settings.DEBUG and settings.ENVIRONMENT == "development":
-#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
