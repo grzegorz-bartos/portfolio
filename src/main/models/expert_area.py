2029d29d59e67e3eda12bb219f4f7f6e7b99189c
Grzegorz-Bartos <grzes.dzes@gmail.com>
2025-03-24 11:33:04 +0100

    feat: add s3 config to expert_area model

--- src/main/models/expert_area.py
+++ src/main/models/expert_area.py
@@ -1,13 +1,16 @@
 from django.core.validators import FileExtensionValidator
 from django.db import models
 
+from main.storages import get_storage
+
 
 class ExpertArea(models.Model):
     title = models.CharField(
         max_length=10,
     )
     image = models.ImageField(
-        upload_to="expert_area_icons",
+        storage=get_storage(),
+        upload_to="media/expert_area_icons",
         blank=True,
         null=True,
         validators=[FileExtensionValidator(["jpg", "png", "jpeg"])],
