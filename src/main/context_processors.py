ad73758743dbae5cadb6aca5ec0e7c453378b035
Grzegorz-Bartos <grzes.dzes@gmail.com>
2025-03-31 16:32:16 +0200

    feat: add PROFILE_PICTURE field to dict in context_processor

--- src/main/context_processors.py
+++ src/main/context_processors.py
@@ -6,8 +6,8 @@ from .models import Profile
 
 def personal_information(request):
     profile = Profile.objects.first()
-    if profile is None:
-        context = {
+    if not profile:
+        return {
             "YEARS_OF_EXPERIENCE": 0,
             "AVAILABILITY": False,
             "GITHUB": "",
@@ -15,15 +15,20 @@ def personal_information(request):
             "DISCORD": "",
             "CLIENT_COUNT": 0,
             "PROJECTS_COMPLETED": 0,
+            "PROFILE_PICTURE": "",
         }
+    if not profile.image:
+        profile_picture_url = ""
     else:
-        context = {
-            "YEARS_OF_EXPERIENCE": date.today().year - profile.start_year,
-            "AVAILABILITY": profile.availability,
-            "GITHUB": profile.github,
-            "LINKEDIN": profile.linkedin,
-            "DISCORD": profile.discord,
-            "CLIENT_COUNT": profile.client_count,
-            "PROJECTS_COMPLETED": profile.projects_completed,
-        }
-    return context
+        profile_picture_url = profile.image.url
+
+    return {
+        "YEARS_OF_EXPERIENCE": date.today().year - profile.start_year,
+        "AVAILABILITY": profile.availability,
+        "GITHUB": profile.github,
+        "LINKEDIN": profile.linkedin,
+        "DISCORD": profile.discord,
+        "CLIENT_COUNT": profile.client_count,
+        "PROJECTS_COMPLETED": profile.projects_completed,
+        "PROFILE_PICTURE": profile_picture_url,
+    }
